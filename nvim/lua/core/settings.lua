8ee7c4d4b57003943680e7366e9ba3d9f5d88009
ibrahim <zeroibrahim49@gmail.com>
2024-07-25 08:21:23 +0300

    removed bg color

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -6,3 +6,11 @@ vim.o.scrolloff = 8
 vim.o.cursorline = false
 vim.o.relativenumber = true
 vim.cmd("set clipboard=unnamedplus")
+-- no background color
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+		vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
+		vim.cmd("hi VertSplit guibg=NONE ctermbg=NONE")
+	end,
+})
